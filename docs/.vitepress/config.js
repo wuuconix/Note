39ac53510513a5df13ebe7691a6c50d62ec04142
wuuconix <1521900139@qq.com>
2022-07-31 09:27:20 +0800

    add sidebar

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,5 +1,17 @@
 export default {
     title: 'Note',
     description: '经典书籍笔记',
-    base: "/note/"
+    base: "/note/",
+    themeConfig: {
+        sidebar: {
+            '/': [
+            {
+                text: 'CSS世界',
+                items: [
+                    { text: '怪异盒子真的怪异吗？', link: '/cssWorld/weird.md' },
+                ]
+            }
+            ],
+        }
+    }
 }
